2c47ebfc1a743cd8566a162d9e448ca7d5a180e3
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-15 13:26:42 +0100

    args -> argv :(

--- FashionMNIST/General/show_pgm.py
+++ FashionMNIST/General/show_pgm.py
@@ -4,8 +4,8 @@ from termshow import show, ANSI_COLOURS
 if __name__ == "__main__":
     import sys
 
-    if len(sys.args) <= 1:
-        print(f"Usage {sys.args[0]} <filename>")
+    if len(sys.argv) <= 1:
+        print(f"Usage {sys.argv[0]} <filename>")
         sys.exit(1)
 
-    show(readpgm(sys.args[1]), ANSICOLOURS)
+    show(readpgm(sys.argv[1]), ANSICOLOURS)
