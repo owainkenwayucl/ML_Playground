cf53f17629924b8387ce7e3721dd0c34d2ff8949
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-16 14:32:53 +0100

    Rewrite.

--- FashionMNIST/Qualcomm/classifier.py
+++ FashionMNIST/Qualcomm/classifier.py
@@ -5,7 +5,6 @@ model = "fashion_MNIST/aic-binary-dir/programqpc.bin"
 def inference(image):
     vit_sess = qaic.Session(model_path = model)
 
-
     vit_sess.setup() 
     input_shape, input_type = vit_sess.model_input_shape_dict['input']
     output_shape, output_type = vit_sess.model_output_shape_dict['output']
@@ -16,14 +15,20 @@ def inference(image):
 
     return output
 
+def main():
+    from imageio import readpgm
+    from termshow import show, ANSI_COLOURS
+    iname = "test.pgm"
 
+    classes = ("T-shirt", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot",)
+
+    if len(sys.argv) > 1:
+        iname = sys.argv[1]
+
+    c, test_image = readpgm(iname)
+    results = inference(test_image)
+ 
+    print(f"Expected: {c}\nPredicted: {classes[np.argmax(results)]}")
 
 if __name__ == "__main__":
-    from imageio import readpgm
-    fname = "test.pgm"
-
-    test_image = readpgm(fname)
-
-    results = inference(test_image)
-
-    print(results)
+    main()
