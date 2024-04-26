d93a05979405c48ac987df2b468ffb9431170855
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-26 16:01:14 +0100

    Split out classifier functions.

---
+++ FashionMNIST/Telum/classifier/fashion_mnist.java
@@ -0,0 +1,68 @@
+package classifier;
+
+import com.ibm.onnxmlir.OMModel;
+import com.ibm.onnxmlir.OMTensor;
+import com.ibm.onnxmlir.OMTensorList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import imageio.pgm;
+
+public class fashion_mnist {
+
+    public static float[] inference(pgm image) {
+        float[][] image_t = image.get_tensor();
+        int width = image_t.length;
+        int height = image_t[0].length;
+        long[] input_shape = {1l,1l,(long)width,(long)height}; 
+        long input_size = (long) width * height;
+        float[] flat_image = flatten_image(image_t);
+
+        // There has to be a neater way!
+        ArrayList<OMTensor> images_ = new ArrayList<OMTensor>();
+        images_.add(new OMTensor(flat_image, input_shape));
+        OMTensorList images = new OMTensorList(images_.toArray(new OMTensor[0]));
+
+        // Do the inference.
+        OMTensor[] output = OMModel.mainGraph(images).getOmtArray();
+        float[] ret_val = output[0].getFloatData();
+
+        return ret_val;
+    }
+
+    public static int max_at(float[] data) {
+        int max_loc = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] > data[max_loc]) {
+                max_loc = i;
+            }
+        }
+
+        return max_loc;
+    }
+
+    public static float[] flatten_image(float[][] image) {
+        int width = image.length;
+        int height = image[0].length;
+        int input_size = width * height;
+
+        float[] flat_image = new float[width*height];
+
+        int inc = 0;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                flat_image[inc] = image[i][j];
+                inc++;
+            }
+        }        
+        return flat_image;
+    }
+
+    public static String classify(float[] probabilities) {
+        String[] fashion_classes = {"T-shirt", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"};
+        String prediction = fashion_classes[max_at(probabilities)];
+        return prediction;
+    }
+}
