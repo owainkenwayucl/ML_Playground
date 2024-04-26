ca694e1cacf023b3c27b50edce26c951a89c314b
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-26 13:52:23 +0100

    First attempt at inference.

--- FashionMNIST/Telum/classifier.java
+++ FashionMNIST/Telum/classifier.java
@@ -2,12 +2,27 @@ import com.ibm.onnxmlir.OMModel;
 import com.ibm.onnxmlir.OMTensor;
 import com.ibm.onnxmlir.OMTensorList;
 
+import java.util.ArrayList;
+import java.util.Array;
+
 import imageio.pgm;
 
 public class classifier {
 
     public static float[] inference(pgm image) {
-        float[] ret_val = {0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+        long[] input_shape = {1l,1l,28l,28l};
+        long input_size = 28l * 28l;
+
+        // There has to be a neater way!
+        ArrayList<OMTensor> images_ = new ArrayList<OMTensor>();
+        images_.add(new OMTensor(image.get_tensor(), input_shape));
+        OMTensorList images = new OMTensorList(images_.toArray(new OMTensor[0]));
+
+        // Do the inference.
+        OMTensorList output = OMModel.mainGraph(images).getOmtArray();
+        float[] ret_val = OMTensorList[0].getFloatData();
+
+        //float[] ret_val = {0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
         return ret_val;
     }
 
