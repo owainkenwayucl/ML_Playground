243ddd55547939c01a430df9233bc405517a9ff9
Owain Kenway <o.kenway@ucl.ac.uk>
2024-04-26 11:31:00 +0100

    WritePGM routine.

--- FashionMNIST/Telum/imageio/pgm.java
+++ FashionMNIST/Telum/imageio/pgm.java
@@ -1,15 +1,26 @@
 package imageio
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class pgm {
-    private[][] image_tensor;
+    private double[][] image_tensor;
+    private String classification;
     
     public pgm() {
         // Nothing!
     }
 
+    public void set_classification(String new_classification) {
+        this.classification = new String(new_classification);
+    }
+
+    public String get_classification(){
+        return new String(this.classification);
+    }
+
     public double[][] get_tensor() {
-        width = this.image_tensor.length;
-        height = this.image_tensor[0].length;       
+        int width = this.image_tensor.length;
+        int height = this.image_tensor[0].length;       
 
         ret_val = new double[width][height];
         for (int i = 0, i<width; i++){
@@ -21,8 +32,8 @@ public class pgm {
     }
 
     public void set_tensor(double[][] new_data) {
-        width = new_data.length;
-        height = new_data[0].length;
+        int width = new_data.length;
+        int height = new_data[0].length;
 
         this.image_tensor = new double[width][height];
 
@@ -35,9 +46,34 @@ public class pgm {
     }
 
     public void write_image(String filename) {
-        width = this.image_tensor.length;
-        height = this.image_tensor[0].length; 
+        int width = this.image_tensor.length;
+        int height = this.image_tensor[0].length; 
+        int white = 255;
+        int quantised = 0;
 
+        try {
+            FileWriter image_file = new FileWriter(filename);
 
+            // Header
+            image_file.write("P2\n");
+            image_file.write("# " + this.classification + "\n");
+            image_file.write(Integer.toString(width) + " " + Integer.toString(height) + "\n");
+            image_file.write(Integer.toString(white) + "\n");
+            
+            // Image
+            for (int j = 0; j < height; j++) {
+                for (int i = 0; j < width; j++) {
+                    quantised = (int) (this.image_tensor[i][j] * white)
+                    imagefile.write(Integer.toString(quantised) + " ")
+                }
+                image_file.write("\n")
+            }
+
+            image_file.close();
+
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + filename)
+            e.printStackTrace();
+        }
     }
 }
