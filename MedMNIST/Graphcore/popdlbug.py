d9e4bf7c96741feb4335b377b396bbb85d36afcc
Owain Kenway <o.kenway@ucl.ac.uk>
2024-07-16 16:57:08 +0100

    Shorter

--- MedMNIST/Graphcore/popdlbug.py
+++ MedMNIST/Graphcore/popdlbug.py
@@ -1,23 +1,18 @@
 import torch
 import poptorch
 import torchvision
-import torch.nn as nn
-from tqdm import tqdm
-
-import numpy
 
 train_dataset = torchvision.datasets.FashionMNIST("data/", transform=torchvision.transforms.ToTensor(), download=True, train=True)
 
-classes = ("T-shirt", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot",)
-
+opts = poptorch.Options()
 
 train_dataloader = poptorch.DataLoader(
     opts, train_dataset, batch_size=128, shuffle=True, num_workers=20
 )
 
-opts = poptorch.Options()
-
 num = 0
 
-for data, labels in tqdm(train_dataloader, desc="batches", leave=False):
-    print(lables.shape)
+for data, labels in train_dataloader:
+    num += labels.shape[0]
+
+print(f"Loaded {num} records from the FashionMNIST training set of 60,000 records."
