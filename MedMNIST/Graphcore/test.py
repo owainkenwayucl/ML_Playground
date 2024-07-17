ae19ab6b84f4f259de0a456f9143ffd543f72de5
Owain Kenway <o.kenway@ucl.ac.uk>
2024-07-17 10:33:58 +0100

    Split batch sizes.
    This is just easier and terrible.

--- MedMNIST/Graphcore/test.py
+++ MedMNIST/Graphcore/test.py
@@ -2,155 +2,156 @@
 # Which is Apache Licensed and  Copyright 2020-2023 MedMNIST Team
 
 # Converted to a script by Dr Owain Kenway, Summer 2024
-if __name__ == '__main__':
 
-    import numpy
-    import torch
-    import torch.nn
-    import torch.utils.data
-    import torch.optim
-    import torchvision.transforms
-    import poptorch
-    import tqdm
+import numpy
+import torch
+import torch.nn
+import torch.utils.data
+import torch.optim
+import torchvision.transforms
+import poptorch
+import tqdm
 
-    import medmnist
+import medmnist
 
-    opts = poptorch.Options()
+opts = poptorch.Options()
 
-    print(f"MedMNIST v{medmnist.__version__} @ {medmnist.HOMEPAGE}")
+print(f"MedMNIST v{medmnist.__version__} @ {medmnist.HOMEPAGE}")
 
-    dataset = "pathmnist"
+dataset = "pathmnist"
 
-    mlbc = "multi-label, binary-class"
+mlbc = "multi-label, binary-class"
 
-    num_epochs = 10
+num_epochs = 10
 
-    batch_size = 4
-    lr = 0.001
+# poptorch causes isssues with batch sizes which are not exact divisors
+train_batch_size = 149
+inference_batch_size = 359
+lr = 0.001
 
-    info = medmnist.INFO[dataset]
-    task = info["task"]
+info = medmnist.INFO[dataset]
+task = info["task"]
 
-    n_channels = info["n_channels"]
-    n_classes = len(info["label"])
+n_channels = info["n_channels"]
+n_classes = len(info["label"])
 
-    data_class = getattr(medmnist, info["python_class"])
+data_class = getattr(medmnist, info["python_class"])
 
-    data_transform = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(mean=[0.5], std=[0.5])
-    ])
+data_transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=[0.5], std=[0.5])
+])
 
-    if task == mlbc:
-        criterion = torch.nn.BCEWithLogitsLoss()
-    else:
-        criterion = torch.nn.CrossEntropyLoss()
+if task == mlbc:
+    criterion = torch.nn.BCEWithLogitsLoss()
+else:
+    criterion = torch.nn.CrossEntropyLoss()
 
-    train = data_class(split="train", transform=data_transform, download=True)
-    test = data_class(split="test", transform=data_transform, download=True)
+train = data_class(split="train", transform=data_transform, download=True)
+test = data_class(split="test", transform=data_transform, download=True)
 
-    train_dataloader = poptorch.DataLoader(opts, dataset=train, batch_size = batch_size, shuffle=True, drop_last=False, num_workers=20)
-    test_dataloader = poptorch.DataLoader(opts, dataset=test, batch_size = batch_size, shuffle=False, drop_last=False)
+train_dataloader = poptorch.DataLoader(opts, dataset=train, batch_size = training_batch_size, shuffle=True, drop_last=False, num_workers=20)
+test_dataloader = poptorch.DataLoader(opts, dataset=test, batch_size = inference_batch_size, shuffle=False, drop_last=False)
 
-    print(train)
-    print(test)
+print(train)
+print(test)
 
-    class classification_model(torch.nn.Module):
-        def __init__(self, in_channels, num_classes):
-            super(classification_model, self).__init__()
+class classification_model(torch.nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(classification_model, self).__init__()
 
-            self.l1 = torch.nn.Sequential(
-                torch.nn.Conv2d(in_channels, 16, kernel_size=3),
-                torch.nn.BatchNorm2d(16),
-                torch.nn.ReLU()
-            )
+        self.l1 = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, 16, kernel_size=3),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU()
+        )
 
-            self.l2 = torch.nn.Sequential(
-                torch.nn.Conv2d(16, 16, kernel_size=3),
-                torch.nn.BatchNorm2d(16),
-                torch.nn.ReLU(),
-                torch.nn.MaxPool2d(kernel_size=2, stride=2)
-            )
+        self.l2 = torch.nn.Sequential(
+            torch.nn.Conv2d(16, 16, kernel_size=3),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        )
 
-            self.l3 = torch.nn.Sequential(
-                torch.nn.Conv2d(16, 64, kernel_size=3),
-                torch.nn.BatchNorm2d(64),
-                torch.nn.ReLU()
-            )
+        self.l3 = torch.nn.Sequential(
+            torch.nn.Conv2d(16, 64, kernel_size=3),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU()
+        )
 
-            self.l4 = torch.nn.Sequential(
-                torch.nn.Conv2d(64, 64, kernel_size=3),
-                torch.nn.BatchNorm2d(64),
-                torch.nn.ReLU()
-            )
+        self.l4 = torch.nn.Sequential(
+            torch.nn.Conv2d(64, 64, kernel_size=3),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU()
+        )
 
-            self.l5 = torch.nn.Sequential(
-                torch.nn.Conv2d(64, 64, kernel_size=3, padding=1),
-                torch.nn.BatchNorm2d(64),
-                torch.nn.ReLU(),
-                torch.nn.MaxPool2d(kernel_size=2, stride=2)
-            )
+        self.l5 = torch.nn.Sequential(
+            torch.nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        )
 
-            self.l6 = torch.nn.Sequential(
-                torch.nn.Linear(64 * 4 * 4, 128),
-                torch.nn.ReLU(),
-                torch.nn.Linear(128, 128),
-                torch.nn.ReLU(),
-                torch.nn.Linear(128, num_classes)
-            )
-            self.loss = criterion
+        self.l6 = torch.nn.Sequential(
+            torch.nn.Linear(64 * 4 * 4, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, num_classes)
+        )
+        self.loss = criterion
 
-        def forward(self, input_, labels=None):
-            input_ = self.l1(input_)
-            input_ = self.l2(input_)
-            input_ = self.l3(input_)
-            input_ = self.l4(input_)
-            input_ = self.l5(input_)
-            input_ = input_.view(input_.size(0), -1)
-            input_ = self.l6(input_)
-            if self.training:
-                return input_, self.loss(input_, labels)
-            return input_
+    def forward(self, input_, labels=None):
+        input_ = self.l1(input_)
+        input_ = self.l2(input_)
+        input_ = self.l3(input_)
+        input_ = self.l4(input_)
+        input_ = self.l5(input_)
+        input_ = input_.view(input_.size(0), -1)
+        input_ = self.l6(input_)
+        if self.training:
+            return input_, self.loss(input_, labels)
+        return input_
 
-    model = classification_model(in_channels = n_channels, num_classes=n_classes)
+model = classification_model(in_channels = n_channels, num_classes=n_classes)
 
-        
-    optimiser = poptorch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    
+optimiser = poptorch.optim.SGD(model.parameters(), lr=lr, momentum=0.9)
 
-    poptorch_model = poptorch.trainingModel(model, options=opts, optimizer=optimiser)
-    model.train()
+poptorch_model = poptorch.trainingModel(model, options=opts, optimizer=optimiser)
+model.train()
 
-    for epoch in range(num_epochs):
-        for inputs, targets in tqdm.tqdm(train_dataloader):
-            if task == mlbc:
-                targets = targets.to(torch.float32)
-            else:
-                targets = targets.squeeze().long()
-            _, loss = poptorch_model(inputs, targets)
+for epoch in range(num_epochs):
+    for inputs, targets in tqdm.tqdm(train_dataloader):
+        if task == mlbc:
+            targets = targets.to(torch.float32)
+        else:
+            targets = targets.squeeze().long()
+        _, loss = poptorch_model(inputs, targets)
 
-    model.eval()
-    guess_true = torch.tensor([])
-    guess_score = torch.tensor([])
+model.eval()
+guess_true = torch.tensor([])
+guess_score = torch.tensor([])
 
-    poptorch_model_inf = poptorch.inferenceModel(model, options=opts)
+poptorch_model_inf = poptorch.inferenceModel(model, options=opts)
 
-    with torch.no_grad():
-        for inputs, targets in test_dataloader:
-            outputs = poptorch_model_inf(inputs)
+with torch.no_grad():
+    for inputs, targets in test_dataloader:
+        outputs = poptorch_model_inf(inputs)
 
-            if task == mlbc:
-                targets = targets.to(torch.float32)
-            else:
-                targets = targets.squeeze().long()
-                targets = targets.float().resize_(len(targets), 1)
+        if task == mlbc:
+            targets = targets.to(torch.float32)
+        else:
+            targets = targets.squeeze().long()
+            targets = targets.float().resize_(len(targets), 1)
 
-            outputs = outputs.softmax(dim=-2)
+        outputs = outputs.softmax(dim=-2)
 
-            guess_true = torch.cat((guess_true, targets),0)
-            guess_score = torch.cat((guess_score, outputs),0)
+        guess_true = torch.cat((guess_true, targets),0)
+        guess_score = torch.cat((guess_score, outputs),0)
 
-    evaluator = medmnist.Evaluator(dataset, "test")
-    metrics = evaluator.evaluate(guess_score)
+evaluator = medmnist.Evaluator(dataset, "test")
+metrics = evaluator.evaluate(guess_score)
 
-    print(metrics)
+print(metrics)
 
