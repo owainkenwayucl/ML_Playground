52eb4d429e954a4454a2101afb1d3b0bd95514b0
Owain Kenway <o.kenway@ucl.ac.uk>
2024-07-17 12:48:00 +0100

    Fix batchs for test at 64

--- MedMNIST/General/test.py
+++ MedMNIST/General/test.py
@@ -21,9 +21,8 @@ mlbc = "multi-label, binary-class"
 
 num_epochs = 10
 
-# poptorch causes isssues with batch sizes which are not exact divisors - need to be fair in other platforms
-train_batch_size = 149
-inference_batch_size = 359
+train_batch_size = 64
+inference_batch_size = 64
 
 lr = 0.001
 
