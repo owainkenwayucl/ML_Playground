8cc79a60b556a10128fe5f5379c6bd6e06dd6eb4
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-13 11:36:53 +0100

    Do train set too

--- MedMNIST/General/export_sets.py
+++ MedMNIST/General/export_sets.py
@@ -25,20 +25,15 @@ def export_sets():
 
     data_class = getattr(medmnist, info["python_class"])
 
-    dataset_names = ['test', 'val']
-    #dataset_names = ['train', 'test', 'val']
+    dataset_names = ['train', 'test', 'val']
 
     ensure_directories(dataset_names)
 
     datasets = {}
 
-    #train = data_class(split="train", download=True, size=224, mmap_mode='r')
-    test = data_class(split="test",  download=True, size=224, mmap_mode='r')
-    val = data_class(split="val",  download=True, size=224, mmap_mode='r')
-
-    #datasets["train"] = train
-    datasets["test"] = test
-    datasets["val"] = val
+    datasets["train"] = data_class(split="train", download=True, size=224, mmap_mode='r')
+    datasets["test"] = data_class(split="test",  download=True, size=224, mmap_mode='r')
+    datasets["val"] = data_class(split="val",  download=True, size=224, mmap_mode='r')
 
     for dataset in dataset_names:
         index = 0
