72199da039f4f8f0c290040ad9e4397c1b95ff68
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-13 10:17:44 +0100

    Start export code

---
+++ MedMNIST/General/export_sets.py
@@ -0,0 +1,56 @@
+# This script exports the MednMNIST datasets into PNGs so they can be imported into non-PyTorch programs.
+
+from PIL import Image
+
+def ensure_directories(output_directories = ['train', 'test', 'val']):
+    import os
+    for a in output_directories:
+        os.makedirs(a, exist_ok=True)
+
+def export_sets():
+    import torch, torchvision
+    import medmnist
+    
+    dataset = "pathmnist"
+
+    info = medmnist.INFO[dataset]
+    task = info["task"]
+
+    n_channels = info["n_channels"]
+    n_classes = len(info["label"])
+
+    data_class = getattr(medmnist, info["python_class"])
+
+    dataset_names = ['train', 'test', 'val']
+
+    ensure_directories(dataset_names)
+
+    datasets = {}
+
+    train = data_class(split="train", download=True, size=224, mmap_mode='r')
+    test = data_class(split="test",  download=True, size=224, mmap_mode='r')
+    val = data_class(split="val",  download=True, size=224, mmap_mode='r')
+
+    datasets["train"] = train
+    datasets["test"] = test
+    datasets["val"] = val
+
+    for dataset in dataset_names:
+        index = 0
+        for element in datasets[dataset]:
+            image = element[0][0]
+            category = classes[element[1]]
+
+            filename = f"{dataset}/{index}.png"
+            print(f"Writing out {filename}")
+
+            temp_i = Image.fromarray(element)
+            temp_i.save(filename)
+            index += 1
+
+
+if __name__ == "__main__":
+    export_sets()
+
+
+
