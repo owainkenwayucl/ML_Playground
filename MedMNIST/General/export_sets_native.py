f12790b1253d52da7ec29e9fec9ad3b62e0f6170
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-16 12:00:40 +0100

    Use MedMNIST's save

---
+++ MedMNIST/General/export_sets_native.py
@@ -0,0 +1,46 @@
+# This script exports the MednMNIST datasets into PNGs so they can be imported into non-PyTorch programs.
+
+from PIL import Image
+
+def ensure_directories(output_directories = ['train', 'test', 'val']):
+    import os
+    for a in output_directories:
+        os.makedirs(a, exist_ok=True)
+
+def str_to_filename(str):
+    return str.replace(" ", "_").replace("/", "_")
+
+def export_sets():
+    import torch, torchvision
+    import medmnist
+    
+    dataset = "pathmnist"
+
+    info = medmnist.INFO[dataset]
+    task = info["task"]
+
+    n_channels = info["n_channels"]
+    n_classes = len(info["label"])
+    classes = info["label"]
+
+    data_class = getattr(medmnist, info["python_class"])
+
+    dataset_names = ['train', 'test', 'val']
+
+    ensure_directories(dataset_names)
+
+    datasets = {}
+
+    datasets["train"] = data_class(split="train", download=True, size=224, mmap_mode='r')
+    datasets["test"] = data_class(split="test",  download=True, size=224, mmap_mode='r')
+    datasets["val"] = data_class(split="val",  download=True, size=224, mmap_mode='r')
+
+
+    for dataset in dataset_names:
+        datasets[dataset].save(dataset)
+
+if __name__ == "__main__":
+    export_sets()
+
+
+
