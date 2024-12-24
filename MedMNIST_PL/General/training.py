b4c74ea3c13880270a8de692a309e3e55eada518
Owain Kenway <o.kenway@ucl.ac.uk>
2024-12-24 15:17:52 +0000

    Missing ,

--- MedMNIST_PL/General/training.py
+++ MedMNIST_PL/General/training.py
@@ -77,7 +77,7 @@ def generate_dataloaders(dataset, batch_size):
     test = data_class(split="test", transform=data_transform, download=True, size=224, mmap_mode='r')
 
     train_dataloader = torch.utils.data.DataLoader(dataset=train, batch_size = batch_size, shuffle=True, num_workers=20, persistant_workers=False)
-    val_dataloader = torch.utils.data.DataLoader(dataset=val, batch_size = batch_size, shuffle=False, num_workers=20 persistant_workers=False)
+    val_dataloader = torch.utils.data.DataLoader(dataset=val, batch_size = batch_size, shuffle=False, num_workers=20, persistant_workers=False)
     test_dataloader = torch.utils.data.DataLoader(dataset=test, batch_size = batch_size, shuffle=False, num_workers=20, persistant_workers=False)
 
     #val_dataloader = None
