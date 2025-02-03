80bbfbf441916d1ab7f27881f05793ccd97c325b
Owain Kenway <o.kenway@ucl.ac.uk>
2025-02-03 15:50:11 +0000

    test inf

--- MedMNIST/Telum/classifier_rebake.py
+++ MedMNIST/Telum/classifier_rebake.py
@@ -4,15 +4,19 @@ import numpy
 import sys
 import argparse
 from image_tools import process_images
+from inference_tools import inference
 
 classes_pathmnist = ('adipose','background','debris','lymphocytes','mucus','smooth muscle','normal colon mucosa','cancer-associated stroma','colorectal adenocarcinoma epithelium')
 
 def main():
     classes = classes_pathmnist
-
+    model =  "MedMNIST/medmnist_classifier_resnet18_pathmnist_55_20_32bit.so"
     filenames = ["MedMNIST/data/test/3153_0_adipose.png","MedMNIST/data/test/3154_0_adipose.png"] # test data
 
     images, labels = process_images(filenames, classes)
+    matched = inference(images, model, classes)
+
+    print(matched)
 
 if __name__ == "__main__":
     main()
