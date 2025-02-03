96183dbde3a1073198eef8cc73e1b3e02bbd3c77
Owain Kenway <o.kenway@ucl.ac.uk>
2025-02-03 16:03:40 +0000

    Start argument parsing

--- MedMNIST/Telum/classifier_rebake.py
+++ MedMNIST/Telum/classifier_rebake.py
@@ -9,9 +9,14 @@ from inference_tools import inference, compare_results
 classes_pathmnist = ('adipose','background','debris','lymphocytes','mucus','smooth muscle','normal colon mucosa','cancer-associated stroma','colorectal adenocarcinoma epithelium')
 
 def main():
+    parser = argparse.ArgumentParser(description="Image Classifier")
+    parser.add_argument("images", nargs="+", type=str, help="A list of images to classify")
+
+    args = parser.parse_args()
+
     classes = classes_pathmnist
     model =  "MedMNIST/medmnist_classifier_resnet18_pathmnist_55_20_32bit.so"
-    filenames = ["MedMNIST/data/test/3153_0_adipose.png","MedMNIST/data/test/3154_0_adipose.png"] # test data
+    filenames = args.images
 
     images, labels = process_images(filenames, classes)
     matched = inference(images, model, classes)
