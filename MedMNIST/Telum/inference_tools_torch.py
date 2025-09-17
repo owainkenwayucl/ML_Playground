79a0ab276e536a04c7a8de86205b7dfae6ea1203
Owain Kenway <o.kenway@ucl.ac.uk>
2025-09-17 14:48:14 +0100

    Start torch implementation

---
+++ MedMNIST/Telum/inference_tools_torch.py
@@ -0,0 +1,51 @@
+import torch
+import torch_nnpa
+import json
+import numpy
+import time
+
+convert_types = {"f32":"float32",
+                 "f16":"float16"}
+
+def inference(image_data, model, classes):
+    setup_start = time.time()
+    model = torch.load(model)
+    model.eval()
+    input_type = "f32"
+
+    images = image_data[0][numpy.newaxis,numpy.newaxis,...].astype(convert_types[input_type])
+    
+    if (len(image_data) > 1):
+        for a in range(1, len(image_data)):
+            image = image_data[a][numpy.newaxis,numpy.newaxis,...].astype(convert_types[input_type])
+            images = numpy.concatenate((images, image), axis=0)
+    imageset = []
+    imageset.append(images)
+    setup_stop = time.time()
+    #print(f"Time in setup: {setup_stop - setup_start}")
+    output = {}
+    inf_start = time.time()
+    output["output"] = model(imageset)
+    inf_stop = time.time()
+    #print(f"Time in inference: {inf_stop - inf_start}")
+
+    matched = match_output(output['output'][0], classes)
+    timing = {"inference_setup":(setup_stop - setup_start), "inference_calc":(inf_stop - inf_start)}
+    return matched, timing
+
+def match_output(probabilities, classes):
+    matched = []
+    for a in range(len(probabilities)):
+        r = classes[numpy.argmax(probabilities[a])]
+        matched.append(r)
+
+    return matched
+
+def compare_results(results,labels):
+    n = len(results)
+    correct = 0
+    for a in range(n):
+        if results[a] == labels[a]:
+            correct +=1
+
+    return 100*(correct/n)
