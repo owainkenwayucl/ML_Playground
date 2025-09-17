ec7a772a1a91c5e8b121fa154725826410b35f81
Owain Kenway <o.kenway@ucl.ac.uk>
2025-09-17 16:32:06 +0100

    Implement

--- MedMNIST/Telum/inference_tools_torch.py
+++ MedMNIST/Telum/inference_tools_torch.py
@@ -1,5 +1,7 @@
-from PyRuntime import OMExecutionSession
+#from PyRuntime import OMExecutionSession
 import json
+import torch
+import torchvision
 import numpy
 import time
 
@@ -8,28 +10,37 @@ convert_types = {"f32":"float32",
 
 def inference(image_data, model, classes):
     setup_start = time.time()
-    session = OMExecutionSession(model)
-    input_signature_json = json.loads(session.input_signature())
-    signature = input_signature_json[0]
-    input_type = signature["type"]
+    #session = OMExecutionSession(model)
+    #input_signature_json = json.loads(session.input_signature())
+    #signature = input_signature_json[0]
+    input_type = "f32"
+    base_model = torchvision.models.resnet152(num_classes=len(classes))
+    base_model.load_state_dict(torch.load(model, weights_only=True))
+    device = torch.device('nnpa')
 
-    images = image_data[0][numpy.newaxis,numpy.newaxis,...].astype(convert_types[input_type])
+    base_model = base_model.to(device)
+    base_model.eval()
+    #images = image_data[0][numpy.newaxis,numpy.newaxis,...].astype(convert_types[input_type])
     
-    if (len(image_data) > 1):
-        for a in range(1, len(image_data)):
-            image = image_data[a][numpy.newaxis,numpy.newaxis,...].astype(convert_types[input_type])
-            images = numpy.concatenate((images, image), axis=0)
-    imageset = []
-    imageset.append(images)
+    #if (len(image_data) > 1):
+    #    for a in range(1, len(image_data)):
+    #        image = image_data[a][numpy.newaxis,numpy.newaxis,...].astype(convert_types[input_type])
+    #        images = numpy.concatenate((images, image), axis=0)
+    #imageset = []
+    #imageset.append(images)
+
+    imageset = torch.from_numpy(numpy.array(images)).to(device)
+
     setup_stop = time.time()
     #print(f"Time in setup: {setup_stop - setup_start}")
-    output = {}
+    #output = {}
     inf_start = time.time()
-    output["output"] = session.run(imageset)
+    #output["output"] = session.run(imageset)
+    probabilities = base_model(imageset).to("cpu")
     inf_stop = time.time()
     #print(f"Time in inference: {inf_stop - inf_start}")
 
-    matched = match_output(output['output'][0], classes)
+    matched = match_output(probabilities, classes)
     timing = {"inference_setup":(setup_stop - setup_start), "inference_calc":(inf_stop - inf_start)}
     return matched, timing
 
